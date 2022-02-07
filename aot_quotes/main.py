a50c7f059d02102060b509564e44fa626ecbbf29
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 11:35:24 +0545

    refactor: change docs url from `/docs` to `/`

--- aot_quotes/main.py
+++ aot_quotes/main.py
@@ -3,10 +3,6 @@ from fastapi import FastAPI
 from aot_quotes.core.quote.views import router as get
 from starlette.responses import RedirectResponse
 
-app = FastAPI(title="Attack on Titan - Quotes API")
+app = FastAPI(title="Attack on Titan - Quotes API", docs_url="/", redoc_url=None)
 
 app.include_router(get, prefix="/random")
-
-@app.get("/")
-async def redirect():
-    return RedirectResponse(url="/docs")
