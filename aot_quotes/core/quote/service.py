0443259b825bada163cfdd58d0ac63a5c382ee5a
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 16:57:44 +0545

    refactor: change the way API sends data in both the endpoints

--- aot_quotes/core/quote/service.py
+++ aot_quotes/core/quote/service.py
@@ -1,9 +1,9 @@
 import random
 import typing as t
-
-from sqlmodel import Session, select
+from email.quoprimime import quote
 
 from aot_quotes.common.db import Quotes
+from sqlmodel import Session, select
 
 
 class GetService:
@@ -13,4 +13,19 @@ class GetService:
             result = s.execute(statement).fetchall()
 
             if result:
-                return random.choice(result)
+                quote_obj = random.choice(result)
+                return (quote_obj[0]).quote
+
+    def get_random_quotes(
+        self, query: int, session: t.Type[Session], engine
+    ) -> t.Optional[dict]:
+        with session(engine) as s:
+            statement = select(Quotes)
+            result = s.execute(statement).fetchall()
+
+            if result:
+                data = list()
+                items = random.sample(result, query)
+                for item in items:
+                    data.append({"id": item[0].id, "quote": item[0].quote})
+                return data
