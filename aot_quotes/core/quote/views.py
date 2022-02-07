3c14556a9aec679adb710b550e40f4533598fc29
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 16:58:13 +0545

    refactor: add some checks for invalid value

--- aot_quotes/core/quote/views.py
+++ aot_quotes/core/quote/views.py
@@ -1,9 +1,8 @@
-from fastapi import Depends
-from fastapi_utils.cbv import cbv
-from fastapi_utils.inferring_router import InferringRouter
-
 from aot_quotes.core.quote.service import GetService
 from aot_quotes.mixins.database import DBMixin
+from fastapi import Depends, HTTPException
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
 
 router = InferringRouter()
 
@@ -15,4 +14,11 @@ class GetCBV(DBMixin):
     @router.get("/")
     def random(self):
         result = self.get_service.get_quote(self.session, self.engine)
+        return {"quote": result}
+
+    @router.get("/int")
+    def random_list(self, query: int):
+        if query < 1:
+            raise HTTPException(status_code=404, detail="Heh? Why?")
+        result = self.get_service.get_random_quotes(query, self.session, self.engine)
         return result
