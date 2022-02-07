801ac8ca47211a2b3349c731aa17669ba1f1e86e
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 11:35:54 +0545

    refactor: remove unnecessary imports

--- aot_quotes/main.py
+++ aot_quotes/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 from aot_quotes.core.quote.views import router as get
-from starlette.responses import RedirectResponse
 
 app = FastAPI(title="Attack on Titan - Quotes API", docs_url="/", redoc_url=None)
 
