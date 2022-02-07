eaed7606ea1177be304e55ddd474175797933b3e
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 18:13:44 +0545

    refactor: bump version to `2.0.0`

--- aot_quotes/main.py
+++ aot_quotes/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from aot_quotes.core.quote.views import router as get
 
-app = FastAPI(title="Attack on Titan - Quotes API", docs_url="/", redoc_url=None)
+app = FastAPI(
+    title="Attack on Titan - Quotes API", docs_url="/", redoc_url=None, version="2.0.0"
+)
 
 app.include_router(get, prefix="/v2/random")
