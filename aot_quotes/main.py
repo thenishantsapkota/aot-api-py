6c94922fd33c2041e28fad08ec5961bceadd0004
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 17:16:42 +0545

    refactor: change url prefix from `/random` to `/v2/random`

--- aot_quotes/main.py
+++ aot_quotes/main.py
@@ -4,4 +4,4 @@ from aot_quotes.core.quote.views import router as get
 
 app = FastAPI(title="Attack on Titan - Quotes API", docs_url="/", redoc_url=None)
 
-app.include_router(get, prefix="/random")
+app.include_router(get, prefix="/v2/random")
